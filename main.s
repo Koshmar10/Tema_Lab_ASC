1e81c12676a8b5b67cc10a60c0d4fe34b9996494
Petru <petru.draghiceanu@gmail.com>
2024-11-25 17:16:32 +0200

    procedura afisare + introducere elemente (succes)

--- main.s
+++ main.s
@@ -1,41 +1,51 @@
 .data
-    hardisk: .space 1024
-    memory_slots: .long 1024
-    formatPrintf: .ascii "%d, "
+    hardisk: .space 32
+    memory_slots: .long 32
+    printElementArray: .asciz "%d, "
+    printEndLine: .asciz "\n"
 .text
+
+afisare_hardisk:
+    push %ebp
+    mov %esp, %ebp
+    push %esi
+    mov 12(%ebp), %esi
+    xor %ecx, %ecx
+afisare:
+    cmp 8(%ebp), %ecx
+    je  end_afisare
+    movb (%esi, %ecx, 1), %al
+    push %ecx
+    push %eax
+    push $printElementArray
+    call printf
+    add $8, %esp
+    popl %ecx
+    inc %ecx
+    jmp afisare
+end_afisare:
+    pop %esi
+    pop %ebp
+    ret
+
 .global main
 
 main:
-        ; initialize the hardisk
-    lea hsrdisk, %esi ; adresa hardisk-ului
-    movl $0, %eax ; valoaera de initializare
-    movl $0, %ecx ; numarul de octeti de initializat
-    movl $memory_slots, %edx ; adresa memoriei
-
-initializare_hardisk:
-    
-    cmp %edx, %ecx  
-    ; verific daca am ajuns la final
-    je end_initializare_hardisk ; daca da ies din bucla
-    mov %eax, (%esi, %ecx, 1) ; initializez fiecare slot din hardisk
-    inc %ecx ; incrementez numarul de octeti
-    jmp initializare_hardisk ; trec la urmatorul slot
-
-end_initializare_hardisk:
-afisare_hardisk:
     lea hardisk, %esi
-    movl $0, %ecx
-loop:
-    cmp $memory_slots, %ecx
-    je  end_loop
-    push $formatScanf
-    push (%esi, %ecx, 1)
-    push %ecx
-    call printf
+    movl $10, %ecx
+    mov $5, (%esi, %ecx, 1)
+    mov $4, %ecx
+    mov $4, (%esi, %ecx, 1)
+    mov $0, %esi
+    pushl $hardisk
+    pushl memory_slots
+    call afisare_hardisk
     add $8, %esp
-    pop %ecx
-    inc %ecx
-end_loop:
+
+cont:
+    push $printEndLine
+    call printf
+    add $4, %esp
 exit:
     movl $1, %eax
     movl $0, %ebx
